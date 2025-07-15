4e73dfd79e817b09420fa7d58522780439e96eb7
laatu08 <partha2002borah@gmail.com>
2025-07-15 18:42:05 +0530

    fintech done

--- FintechService/src/index.ts
+++ FintechService/src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import registerRoutes from "./routes/register";
 import verifyOtpRoutes from "./routes/verifyOtp";
+import dataRoute from "./routes/data"
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/fintech/register", registerRoutes);
 app.use("/fintech/verify-otp", verifyOtpRoutes);
+app.use("/fintech/data",dataRoute);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
