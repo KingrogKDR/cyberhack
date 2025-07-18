aa9eeadc9bc3cb9968c6ec47e3093825f82acdb2
KingrogKDR <abhisheksaikia766@gmail.com>
2025-07-18 20:28:00 +0530

    logger updated

--- vault/src/index.ts
+++ vault/src/index.ts
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
-import pinoHTTP from "pino-http";
 import adminRouter from "./routes/admin.route";
 import tokenRouter from "./routes/token.route";
-import logger from "./utils/logger";
 
 dotenv.config({
   path: path.resolve(__dirname, "../../.env"),
@@ -12,7 +10,6 @@ dotenv.config({
 
 const app = express();
 app.use(express.json());
-app.use(pinoHTTP({ logger })); // logs all HTTP requests
 
 // routes
 app.use("/api/v1", tokenRouter);
